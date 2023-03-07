11929236f3ecdd7861422897d081ba283edb740c
Jonathan Peng <jonathan.peng.dfw@gmail.com>
2023-03-07 04:59:21 +0000

    Updated db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -33,13 +33,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_081128) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
-  create_table "course_users", force: :cascade do |t|
-    t.integer "course_id"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "consultations", force: :cascade do |t|
     t.string "title"
@@ -49,7 +42,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_081128) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
+  create_table "course_users", force: :cascade do |t|
+    t.integer "course_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "courses", force: :cascade do |t|
     t.string "department"
     t.integer "code"
@@ -64,14 +64,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_081128) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "hour"
-    t.string "role"
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
