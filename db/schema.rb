4d01a774425deb62cc75c2e18bce2ab017ee5bb8
root <root@localhost>
2023-03-23 18:50:48 +0000

    updated new user form and also added dropdown for role

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_06_081128) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_23_184158) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,12 +58,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_081128) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "status"
     t.integer "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "hour"
+    t.string "role"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "status"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
