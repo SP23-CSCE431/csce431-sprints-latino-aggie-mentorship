9ff603ce4354e15db387c7f1dc3909f2252c4296
root <root@localhost>
2023-04-03 20:59:27 +0000

    adding the leader board functionallty

---
+++ app/controllers/leader_controller.rb
@@ -0,0 +1,17 @@
+class LeaderController < ApplicationController
+    # this controller list the users by how many hours they have done 
+    def index 
+    @ranking =ranker()
+    end 
+
+    private  # creating a private function will allow only the classes to access the function + it will not create a view for it 
+    def ranker
+        variables = []
+        User.where(role: "Mentor").all.each do |user| # looping through the users ".each"
+            count = user.hour # only gets the number of hours where its true 
+            variables.push([user,count]) # push it to the array 
+        end
+        # sort_by automatic ruby function 
+        return variables.sort_by{|var| var[1]}.reverse
+    end
+end
