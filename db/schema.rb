59b1d4aad43e7474f331d9f60a42d8bbd8d9fb8e
root <root@localhost>
2023-04-04 02:07:34 +0000

    added default value of 0 for user hour

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_02_202453) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_04_020138) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,7 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_02_202453) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.integer "hour"
+    t.integer "hour", default: 0
     t.string "role"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
