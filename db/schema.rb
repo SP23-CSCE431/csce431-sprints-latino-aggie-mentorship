108399e11d300ae675102e97d7ac5bd4e1ed4523
root <root@localhost>
2023-04-10 01:48:43 +0000

    fixed migration files for hobby_id and user_id in hobby_users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_09_065648) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_10_014515) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_09_065648) do
   create_table "hobby_users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "hobby_id"
+    t.integer "user_id"
   end
 
   create_table "user_events", force: :cascade do |t|
