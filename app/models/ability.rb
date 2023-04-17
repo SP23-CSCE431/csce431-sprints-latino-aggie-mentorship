0099b2e354cad0cad39c730a407c7ae096d40c5c
root <root@localhost>
2023-04-17 19:50:07 +0000

    tentative mentor tutoring hour bug fix

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,12 +5,12 @@ class Ability
         if admin.admin?
             can :manage, :all
         elsif admin.mentor?
-            can :manage, User, id: admin.id
+            can :manage, User, id: User.find_by(email:admin.email).id
             can :manage, :add_hours, :log_hours
             can :manage, :check_code
             can :read, Consultation
         elsif admin.mentee?
-            can :manage, User, id: admin.id
+            can :manage, User, id: User.find_by(email:admin.email).id
             can :manage, :check_code
             can :read, Consultation
         else
